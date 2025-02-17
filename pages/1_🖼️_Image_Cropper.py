6c2553a5792626dfc0c847736ae62dc48be3da1a
jakobjspranger <jakobspranger@gmx.de>
2025-02-17 18:30:11 +0100

    Changes to tab header "Jakob's Portfolio"

--- pages/1_🖼️_Image_Cropper.py
+++ pages/1_🖼️_Image_Cropper.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 # ----- Page configs -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Jakob's Portfolio",
     page_icon="📊",
 )
 
--- pages/3_🌦️_Temperatures_Dashboard.py
+++ pages/3_🌦️_Temperatures_Dashboard.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # ----- Page configs -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Jakob's Portfolio",
     page_icon="📊",
 )
 
